fe52f0926663ec48edd4ea4e40ccfa6b3fb7ed09
tarked <zayfnmail@gmail.com>
2024-07-08 20:22:40 -0400

    Update NoSmoke.bat

--- NoSmoke.bat
+++ NoSmoke.bat
@@ -1,59 +1,31 @@
 @echo off
 color 6
 
-title NOSMOKING
+title 
 
 :greeting
 cls
-echo 
-echo                           .,aad88888888888baa,.
-echo                      ,ad8888888888888888888888888ba,.
-echo                  ,ad888888888888888888888888888888888ba,
-echo               ,ad888888888P""'            """Y88888888888ba.
-echo             ,d88888888P""                       ""Y888888888ba
-echo           a888888888"                               ""Y88888888b,
-echo         ,888888888b,                                   ""Y8888888b,
-echo        d888888888888b,                                    "Y8888888b,
-echo      ,8888888' "888888b,                                    "Y8888888b
-echo     ,888888"     "Y88888b,                                    "Y888888b
-echo    ,888888'        "Y88888b,                                    "888888b
-echo   ,888888'     a,  8a"Y88888b,                                   `888888a
-echo  ,888888'      `8, `8) "Y88888b,                  ,adPPRg,        `888888,
-echo  888888'        8)  ]8   "Y88888b,            ,ad888888888b        Y88888b
-echo d88888P        ,8' ,8'     "Y88888b,      ,gPPR888888888888        `888888,
-echo 888888'       ,8' ,8'        "Y88888b,,ad8""   `Y888888888P         )88888)
-echo 888888        8)  8)           "Y888888"        (8888888""          (88888)
-echo 888888        8,  8,          ,ad8Y88888b,      d888""              d88888)
-echo 888888        `8, `8,     ,ad8""   "Y88888b,,ad8""                  888888)
-echo 888888         `8, `" ,ad8""         "Y88888b"                     ,888888'
-echo Y88888,           ,gPPR8b           ,ad8Y88888b,                   d888888
-echo `88888b          dP:::::Yb      ,ad8""   "Y88888b,                ,888888P
-echo  888888,         8):::::(8  ,ad8""         "Y88888b,              d888888'
-echo  `888888,        Yb:;;;:d888""               "Y88888b,           d888888P
-echo   Y888888,        "8ggg8P"                     "Y88888b,       ,d888888P
-echo    Y88888b,                                      "Y88888b,    ,8888888"
-echo     Y88888b,                                       "Y88888b, d8888888"
-echo      Y888888,                                        "Y888888888888P'
-echo       "888888b,                                        "8888888888"
-echo         Y888888b,                                     ,888888888"
-echo           Y8888888ba,                              ,a888888888"
-echo             "Y88888888ba,._                   .,ad888888888P"
-echo                "Y88888888888bbaa,,_____,,aadd88888888888""
-echo                    "Y8888888888888888888888888888888""  
-echo                        ""Y888888888888888888888P""       
-echo 000000000000000000000000000000 Made By Mup 000000000000000000000000000000
-echo 0000000000000000000000000000000000000000000000000000000000000000000000000 
-echo 
-echo               .M"""bgd                         `7MM              
-echo              ,MI    "Y                           MM              
-echo              `MMb.    `7MMpMMMb.pMMMb.  ,pW"Wq.  MM  ,MP'.gP"Ya  
-echo                `YMMNq.  MM    MM    MM 6W'   `Wb MM ;Y  ,M'   Yb 
-echo              .     `MM  MM    MM    MM 8M     M8 MM;Mm  8M"""""" 
-echo              Mb     dM  MM    MM    MM YA.   ,A9 MM `Mb.YM.    , 
-echo              P"Ybmmd" .JMML  JMML  JMML.`Ybmd9'.JMML. YA.`Mbmmd' 
-echo 0000000000000000000000000000000000000000000000000000000000000000000000000
-echo 0000000000000000000000000000000000000000000000000000000000000000000000000
-set /p Ip=ENTER IP: 
+echo     888      d8b                           d88P   d88P 
+echo     888      Y8P                          d88P   d88P  
+echo     888                                  d88P   d88P   
+echo .d88888      888      88888b.           d88P   d88P    
+echo d88" 888      888      888 "88b         d88P   d88P     
+echo 888  888      888      888  888        d88P   d88P      
+echo Y88b 888      888      888  888       d88P   d88P    
+echo  "Y88888      888      888  888      d88P   d88P     
+echo
+echo
+echo                  welcome to din market
+echo
+echo                    your balance- 16,9712$
+echo
+echo                    listings 
+echo                    1 weed (5g) - 40$
+echo                    2 lsd (50)- 70$
+echo                    3 heroione (7g) - 80$
+echo                   --------------------------------
+
+set /p Ip=: 
 :top
 PING -n 1 %IP% | FIND "TTL="
 IF ERRORLEVEL 1 (SET in=0b & echo IP Is SMOKED.) 
